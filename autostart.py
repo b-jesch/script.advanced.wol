22aa9e3bfb85ce20c0913e23a12b8dd6b9758e0e
b-jesch <birger.jesch@gmail.com>
2021-01-18 17:59:21 +0100

    autostart.py modified, changelog.txt removed

--- autostart.py
+++ autostart.py
@@ -10,18 +10,18 @@ addon = xbmcaddon.Addon()
 addon_id = addon.getAddonInfo('id')
 version = addon.getAddonInfo('version')
 
-autostart = addon.getSetting("autostart")
-wolAfterStandby = addon.getSetting("wolAfterStandby")
+autostart = True if addon.getSetting("autostart").lower() == 'true' else False
+wolAfterStandby = True if addon.getSetting("wolAfterStandby").lower() == 'true' else False
 wolDelayOnLaunch = int(addon.getSetting("wolDelayOnLaunch"))
 wolDelayAfterStandby = int(addon.getSetting("wolDelayAfterStandby")) * 1000
 
-if autostart.lower() == "true":
+if autostart:
     import default
 
     if wolDelayOnLaunch > 0:
         xbmc.sleep(wolDelayOnLaunch)
     default.main(True)
-    if wolAfterStandby.lower() == "true":
+    if wolAfterStandby:
         xbmc.log('[{} {}]: Waiting for resume from standby'.format(addon_id, version), xbmc.LOGDEBUG)
         previousTime = int(time.time())
         while not xbmc.Monitor().abortRequested:
