9a159121064a671166ec9dbd72abf313de047ec6
b-jesch <birger.jesch@gmail.com>
2024-02-09 16:19:52 +0100

    improved standby code

--- autostart.py
+++ autostart.py
@@ -11,6 +11,7 @@ version = addon.getAddonInfo('version')
 autostart = True if addon.getSetting("autostart").lower() == 'true' else False
 wolAfterStandby = True if addon.getSetting("wolAfterStandby").lower() == 'true' else False
 
+mon = xbmc.Monitor()
 if autostart:
 
     import default
@@ -24,20 +25,17 @@ if autostart:
     if wolAfterStandby:
         previousTime = int(time.time())
 
-        while True:
-            elapsedTime = int(time.time()) - previousTime
+        while not mon.abortRequested():
 
             # if elapsedTime > 60 secs possibly there was an inactive state between (standby?)
 
-            if elapsedTime > 60:
-                if int(addon.getSetting("wolDelayAfterStandby")) > 0:
-                    xbmc.sleep(wolDelayAfterStandby * 1000)
+            if int(time.time()) - previousTime > 60:
+                if int(addon.getSetting("wolDelayAfterStandby")) > 0: xbmc.sleep(wolDelayAfterStandby * 1000)
 
                 default.log('Start WOL after standby (sleeped {} secs)'.format(int(time.time() - previousTime)))
                 default.main(True)
 
             previousTime = int(time.time())
-            xbmc.Monitor().waitForAbort(10)
-            if xbmc.Monitor().abortRequested: break
+            mon.waitForAbort(10)
 
-    default.log('abort requested', xbmc.LOGINFO)
+        default.log('abort requested', xbmc.LOGINFO)
